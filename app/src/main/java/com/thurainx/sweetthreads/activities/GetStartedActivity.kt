6771c563bebe6eda03713dd056922d3436b62c04
thurain <thurainbowin.dev@gmail.com>
2022-10-14 19:48:01 +0630

    - getting start screen ui

---
+++ app/src/main/java/com/thurainx/sweetthreads/activities/GetStartedActivity.kt
@@ -0,0 +1,12 @@
+package com.thurainx.sweetthreads.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.thurainx.sweetthreads.R
+
+class GetStartedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get_started)
+    }
+}
